06e93f455d64d56a7b3740da2a61bb2babe57aed
up202010351 <72741398+joaomicaelocosta@users.noreply.github.com>
2023-02-05 01:07:44 +0000

    project setup

---
+++ app/movies/page.tsx
@@ -0,0 +1,8 @@
+import '@/styles/modules/movie.modules.scss'
+
+export default function Movies() {
+  return (
+    <main>
+    </main>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import '@/styles/modules/page.modules.scss'
+
+export default function Home() {
+  return (
+    <main>
+    </main>
+  )
+}
